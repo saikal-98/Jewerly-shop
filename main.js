500477fc690224dd489a39a66ad513a56fac2197
Saikal <saikal-98@gmail.com>
2023-03-06 19:33:03 +0600

    navbar

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 let api = "http://localhost:8000/juverely";
+
+// search start
+let searchInp = document.querySelector(".nav-inp");
+let searchBtn = document.querySelector(".search-btn");
+
+let searchVal = "";
+// search end
